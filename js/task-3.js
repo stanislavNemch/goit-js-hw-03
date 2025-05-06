5ee11ad9a0035c0faf5ce90dc660fa317a078bd1
Stanislav Nemchenko <stas.buka@gmail.com>
2025-05-07 00:19:07 +0300

    task-2 and task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,16 @@
-function checkForSpam(message) {
-    // Приводимо рядок до нижнього регістру для перевірки
-    const normalizedMessage = message.toLowerCase();
-
-    // Перевіряємо наявність заборонених слів "spam" або "sale"
-    return (
-        normalizedMessage.includes("spam") || normalizedMessage.includes("sale")
-    );
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
+    for (const number of numbers) {
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    }
+    return filteredNumbers;
 }
 
-// Приклади використання
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// Перевірка роботи функції
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
